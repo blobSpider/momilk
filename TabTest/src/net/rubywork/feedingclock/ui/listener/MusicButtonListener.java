af1dcd670fda3f3322596d9ffdf0e684cf3d4b09
JaeKyungLee <skullee84@gmail.com>
2013-03-05 09:44:51 +0900

    SoundTest

--- TabTest/src/net/rubywork/feedingclock/ui/listener/MusicButtonListener.java
+++ TabTest/src/net/rubywork/feedingclock/ui/listener/MusicButtonListener.java
@@ -1,17 +1,18 @@
 package net.rubywork.feedingclock.ui.listener;
 
 import net.rubywork.feedingclock.R;
-import android.media.AudioManager;
-import android.media.SoundPool;
+import net.rubywork.feedingclock.ui.support.AppContext;
+import android.media.MediaPlayer;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class MusicButtonListener implements OnClickListener {
 	
-	private SoundPool soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+	private MediaPlayer whiteNoise = MediaPlayer.create(AppContext.getInstance().getMainActivity(), R.raw.whitenoise);
 	
 	@Override
 	public void onClick(View view) {
-		this.soundPool.play(soundPool.load(view.getContext(), R.raw.tictok, 1), 1, 1, 0, 0, 1);
+		whiteNoise.start();
 	}
+
 }
