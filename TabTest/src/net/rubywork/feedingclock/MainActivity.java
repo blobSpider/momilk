572f1440f46ec116110c049fbc5458f534086a3d
ruby <ruby69@gmail.com>
2013-03-06 10:09:30 +0900

    fixed the bug of the main-menu button.

--- TabTest/src/net/rubywork/feedingclock/MainActivity.java
+++ TabTest/src/net/rubywork/feedingclock/MainActivity.java
@@ -18,19 +18,22 @@ import net.rubywork.feedingclock.ui.support.LastFeedingRecordThread;
 import net.rubywork.feedingclock.ui.view.MainMenuButton;
 import net.rubywork.feedingclock.ui.view.SubMenuButton;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 	{
-		AppContext.getInstance().setMainActivity(this); // * important : First of all, it must do invocation!
+		// AppContext.getInstance().setMainActivity(this); // * important :
+		// First of all, it must do invocation!
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		AppContext.getInstance().setMainActivity(this);
 		setContentView(R.layout.main);
 		init();
 	}
@@ -46,29 +49,30 @@ public class MainActivity extends Activity {
 		initListeners();
 	}
 
-	private static final int [] MENU_BUTTON_IDs = {R.id.menuSettingsButton, R.id.menuMusicButton, R.id.menuHistoryButton, R.id.menuRecordButton};  // it determined buttons order for displaying.
-	private static final int [] CONTAINER_VIEW_IDs = {R.id.moreContainer, R.id.soundContainer, R.id.historyContainer, R.id.feedingContainer};
-	
+	private static final int[] MENU_BUTTON_IDs = { R.id.menuSettingsButton, R.id.menuMusicButton, R.id.menuHistoryButton, R.id.menuRecordButton }; // it determined buttons order for displaying.
+	private static final int[] CONTAINER_VIEW_IDs = { R.id.moreContainer, R.id.soundContainer, R.id.historyContainer, R.id.feedingContainer };
+
 	private void initView() {
 		initMenuButtonAndContainerView();
 	}
-	
-	private void initMenuButtonAndContainerView(){
+
+	private void initMenuButtonAndContainerView() {
 		SubMenuButton[] subButtons = new SubMenuButton[MENU_BUTTON_IDs.length];
 		MainMenuButton mainButton = (MainMenuButton) findViewById(R.id.mainButton);
 		mainButton.setImageView((ImageView) findViewById(R.id.mainButtonImage));
 		mainButton.setButtons(subButtons);
-		
+
 		Map<SubMenuButton, MenuViewEntry> menuButtonViewMap = new HashMap<SubMenuButton, MenuViewEntry>();
 		SubMenuButtonListener subMenuButtonListener = new SubMenuButtonListener(mainButton, menuButtonViewMap);
-		MenuViewCallback [] menuViewCallbacks = {null, null, new HistoryMenuViewCallback(), null};
-		
-		for(int i = 0; i<MENU_BUTTON_IDs.length; i++){
-			subButtons[i] = (SubMenuButton) findViewById(MENU_BUTTON_IDs[i]);
-			menuButtonViewMap.put(subButtons[i], new MenuViewEntry(findViewById(CONTAINER_VIEW_IDs[i]), menuViewCallbacks[i]));
+		MenuViewCallback[] menuViewCallbacks = { null, null, new HistoryMenuViewCallback(), null };
 
+		for (int i = 0; i < MENU_BUTTON_IDs.length; i++) {
+			subButtons[i] = (SubMenuButton) findViewById(MENU_BUTTON_IDs[i]);
+			subButtons[i].initView(mainButton);
 			subButtons[i].initAnimation(subButtons.length, i);
 			subButtons[i].setOnClickListener(subMenuButtonListener);
+			
+			menuButtonViewMap.put(subButtons[i], new MenuViewEntry(findViewById(CONTAINER_VIEW_IDs[i]), menuViewCallbacks[i]));
 		}
 	}
 
@@ -90,5 +94,22 @@ public class MainActivity extends Activity {
 		changeButton.setOnClickListener(changeButtonListener);
 
 		findViewById(R.id.play).setOnClickListener(new MusicButtonListener());
+	}
+
+	@Override
+	public void onBackPressed() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("종료하시겠습니까?").setCancelable(false).setPositiveButton("예", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				finish();
+				System.exit(0);
+			}
+		}).setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.cancel();
+			}
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
 	}
 }
